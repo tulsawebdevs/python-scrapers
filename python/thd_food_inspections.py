33329337728e326831ea4c607b7323b9d7d27acb
groovecoder <luke.crouch@gmail.com>
2012-01-14 10:22:05 -0600

    use paging

--- python/thd_food_inspections.py
+++ python/thd_food_inspections.py
@@ -15,14 +15,16 @@ import requests
 from pyquery import PyQuery as pq
 
 THD_ROOT = 'http://tulsa.ok.gegov.com/tulsa'
+PAGE_SIZE = 20
+SECONDS_THROTTLE = 1
 
-SEARCH_DATA = {'startrow': 1,
+SEARCH_PARAMS = {'startrow': 1,
                'source': 'quick',
                'precision': 'LIKE',
                'startDate': '01-07-2011',
                'endDate': '01-07-2012',
                'establishmentClass': 'ANY',
-               'maxrows': 10,
+               'maxrows': PAGE_SIZE,
                'filter': 'est',
                'Search': 'Search'}
 
@@ -33,10 +35,10 @@ def grouper(iterable, n, fillvalue=None):
     return izip_longest(*args, fillvalue=fillvalue)
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-    search_resp = requests.post(THD_ROOT + '/index.cfm', data=SEARCH_DATA)
+def scrape_inspections(startrow):
+    SEARCH_PARAMS.update({'startrow': startrow})
+    search_resp = requests.post(THD_ROOT + '/index.cfm', data=SEARCH_PARAMS)
+    doc = pq(search_resp.content)
     facility_links = pq(search_resp.content).find(
         'div#searchResults a.resultMore')
     for f_link in facility_links:
@@ -69,7 +71,21 @@ def main(argv=None):
                 scraperwiki.sqlite.save(unique_keys=['name', 'date'],
                                         data=inspection)
 
-            time.sleep(1)
+            time.sleep(SECONDS_THROTTLE)
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    search_resp = requests.post(THD_ROOT + '/index.cfm', data=SEARCH_PARAMS)
+    doc = pq(search_resp.content)
+    resultsHeader = pq(doc).find('#searchResultsHeader')
+    total_results = pq(resultsHeader.find('strong')[2]).text()
+    print "Total Results: %s " % total_results
+    for startrow in range(1, int(total_results) + PAGE_SIZE, PAGE_SIZE):
+        print "Scraping from %s to %s" % (startrow, startrow + PAGE_SIZE)
+        scrape_inspections(startrow)
+
 
 if RUNNING_IN_SCRAPERWIKI:
     main()
