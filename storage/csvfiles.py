b638e0c2b385253d3d40ef45024b75b4d4835ccb
groovecoder <luke.crouch@gmail.com>
2012-02-01 00:24:16 -0600

    tighter writeheader code

--- storage/csvfiles.py
+++ storage/csvfiles.py
@@ -11,20 +11,16 @@ if 'STORE_TO_CSV' in os.environ:
     facilities_writer = csv.DictWriter(facilities_file, ['url', '_id', 'type',
                             'name', 'location', 'latitude', 'longitude'])
     # HACK: 2.6 doesn't have writeheader() method :(
-    headers = {}
-    for field in facilities_writer.fieldnames:
-        headers[field] = field
-    facilities_writer.writerow(headers)
+    facilities_writer.writerow(dict((field,field)
+                              for field in facilities_writer.fieldnames))
 
     inspections_file = open('thd_inspections.csv', 'wb')
     inspections_writer = csv.DictWriter(inspections_file, ['facility', 'url',
                             '_id', 'date', 'priority', 'purpose', 'result',
                             'actions'])
     # HACK: 2.6 doesn't have writeheader() method :(
-    headers = {}
-    for field in inspections_writer.fieldnames:
-        headers[field] = field
-    inspections_writer.writerow(headers)
+    inspections_writer.writerow(dict((field, field)
+                                for field in inspections_writer.fieldnames))
     STORING_TO_CSV = True
     print "Storing to csv"
 
