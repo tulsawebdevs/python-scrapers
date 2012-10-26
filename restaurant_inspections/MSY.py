6077ea4ac3d1afeed4cad28fe3a19719d46238e8
jdungan <johnadungan@gmail.com>
2012-10-25 23:13:07 -0500

    Some clean up of New Orleans

--- restaurant_inspections/MSY.py
+++ restaurant_inspections/MSY.py
@@ -18,14 +18,14 @@ URL_ROOT = 'http://inspections.eatsafe.la.gov'
 SECONDS_THROTTLE = 3
 
 
-BRK = "==============================================\n\n"
+BRK = "==============================================\n"
 
 def main(argv=None):
     fields = ['url', 'name', 'location', 'city', 'zip', 'date', 'score']
     filename = 'whoodat.csv'
     csv_writer = get_csv_writer(filename, fields)
     
-    page_number=189
+    page_number=0
     more_to_do=True
 
 #   Have to use mechanize to get past the pain in the ASPX form handling
@@ -38,15 +38,13 @@ def main(argv=None):
         # SEARCH_PARAMS['pageIndex']=page_number
         search_url = "%s%s%i%s" % (URL_ROOT, '/Results.aspx?pageIndex=',page_number,'&pageSize=150')
         search_resp = br.open(search_url)
-        soup=BeautifulSoup(search_resp.read())
-        rest_rows=soup.find_all('tr',id='MainContent_ResultGrid_ctl00__1')
-        for rest_row in rest_rows:
-            print rest_row.prettify()
+        content=search_resp.read()
+        soup=BeautifulSoup(content)
 
         if soup.find('div',text=re.compile('No records to display')):
             more_to_do=False
         else:
-            # facilities=_scrape_content(search_resp.content, csv_writer)
+            facilities=_scrape_content(content, csv_writer)
             time.sleep(SECONDS_THROTTLE)
             page_number=page_number+1
 
@@ -55,26 +53,8 @@ def main(argv=None):
 
 def _scrape_content(content, csv_writer):
 
-
     soup=BeautifulSoup(content)
     result_table=soup.find('table',id=re.compile("MainContent_ResultGrid_ctl00"))
-    # <tr class="rgRow" id="MainContent_ResultGrid_ctl00__12">
-    #     <td>
-    #         <a href="Summary.aspx?accountID=10-0070535&amp;pageIndex=0&amp;pageSize=20&amp;%20All=" title="121 ARTISAN BISTRO">121 ARTISAN BISTRO</a>
-    #     </td>
-    #     <td>
-    #         121 DR. MICHAEL DEBAKEY DRIVE
-    #     </td>
-    #     <td>
-    #         LAKE CHARLES
-    #     </td>
-    #     <td>
-    #         Calcasieu
-    #     </td>
-    #     <td>
-    #         70601
-    #     </td>
-    # </tr>
     
     for each_row in result_table.find_all('tr',id=re.compile('MainContent_ResultGrid')):
         facility={}
@@ -92,7 +72,7 @@ def _scrape_content(content, csv_writer):
         time.sleep(SECONDS_THROTTLE)
         facility_soup = BeautifulSoup(facility_resp.content)
         
-        print "========="
+        print BRK
         last_inspection=facility_soup.find('tr',id=re.compile("MainContent_InspectionGrid_ctl00__0"))
         
         if last_inspection:
